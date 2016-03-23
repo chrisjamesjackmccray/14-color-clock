a04a7753150ea5c7dba076b3d7c15ce213024c3f
James McCray <chrisjamesjackmccray@gmail.com>
2016-03-23 14:01:29 -0500

    Assignment Commit

--- app/app.js
+++ app/app.js
@@ -12,20 +12,44 @@ console.log("in app!");
      clockdiv.innerHTML = hours + ":" + minutes + ":" + seconds; {
     }
 
-   function checkTime(hours) {
-     if (hours < 10) {
-      hours = "0" + hours}
-      return hours;
-    }
+    if (minutes < 10) {
+     minutes = '0' +  minutes;
+   }
+   if (seconds < 10) {
+     seconds = '0' + seconds;
+   }
 
-    else if (minutes < 10)
-      minutes = "0" + minutes; {
-      return minutes;
-    }
+
+
+
+   if (hours > 12) {
+   hours = hours - 12;
+   }
+
+   if (minutes > 60) {
+   minutes = minutes - 60;
+   }
 
   }
 
 
+function lineLength() {
+ let line = document.getComputedStyle("hr-width");
 
 
-  setInterval(displayTime, 1000);
+ if (minutes === 60) {
+   return lineLength() === 100%;
+ }
+
+
+
+}
+
+
+
+
+
+
+
+
+setInterval(displayTime, 1000);
