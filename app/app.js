7f1cedbe0283e1062bdd48bcf83258cce9f35c4b
James McCray <chrisjamesjackmccray@gmail.com>
2016-03-23 15:26:15 -0500

    Assignment Commit

--- app/app.js
+++ app/app.js
@@ -7,34 +7,34 @@ console.log("in app!");
     let minutes = currentTime.getMinutes();
     let seconds = currentTime.getSeconds();
 
-
-    let clockdiv = document.querySelector("#clock");
-     clockdiv.innerHTML = hours + ":" + minutes + ":" + seconds; {
+    if (hours > 12) {
+      hours = hours - 12;
     }
 
     if (minutes < 10) {
-     minutes = '0' +  minutes;
-   }
-   if (seconds < 10) {
+      minutes = '0' +  minutes;
+    }
+
+    if (seconds < 10) {
      seconds = '0' + seconds;
-   }
+    }
+
+    if (hours < 10) {
+     hours = '0' + hours;
+    }
+
+    let clockdiv = document.querySelector("#clock");
+    clockdiv.innerHTML = hours + ":" + minutes + ":" + seconds;
 
 
 
 
-   if (hours > 12) {
-   hours = hours - 12;
-   }
 
-   if (minutes > 60) {
-   minutes = minutes - 60;
-   }
 
   }
 
 
 setInterval(displayTime, 1000);
 
-document.querySelector("#line"); {
- let line = style.width + currentTime() / 60 * 100;/
-}
+// document.querySelector("#line");
+// let line = style.width + currentTime() / 60 * 100;
